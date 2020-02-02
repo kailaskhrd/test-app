8de6c1984fcf64a7c345e75458cfd4ca0012bc63
kailas <kailaskhrd@gmail.com>
2020-02-02 12:29:35 +0530

    [created user list + added bootstrap]

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
 gem "pundit"
+gem 'will_paginate', '~> 3.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+
   get 'home/index'
   root to: "home#index"
 
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
-
+  resources :users, only:[:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
