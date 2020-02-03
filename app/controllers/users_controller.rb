04d7638c83f908b9eaa08b9220a33de144d1ee57
kailas <kailaskhrd@gmail.com>
2020-02-04 01:59:51 +0530

    address update option to admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,25 @@ class UsersController < ApplicationController
     @users = @users.paginate(:page => params[:page], :per_page => 5)
   end
 
-  def show
-    @user = User.find(params[:id])
-    authorize @user
-  end
-
   def edit
-
+    authorize current_user
   end
 
   def update
-
+    authorize current_user
+    if @user.update(user_update_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
-    def user_params
-      params.require(:user).permit(policy(@user || User).permitted_attributes)
+    def user_update_params
+      params.require(:user).permit(policy(User).permitted_update_attributes)
+    end
+
+    def set_user
+      @user = User.all_users.where(id: params[:id]).first
     end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,11 +4,16 @@ class UserPolicy < ApplicationPolicy
     current_user.has_right?('view users list')
   end
 
-  def show?
-    current_user.has_right?('view users list')
+  def edit?
+    current_user.has_right?('update all user addresses')
   end
 
-  def permitted_attributes
-    [:address_attributes=> [:line_1, :line_2, :city, :state, :pincode, :landmark]]
+  def update?
+    current_user.has_right?('update all user addresses')
   end
+
+  def permitted_update_attributes
+    [:address_attributes=> [:id, :line_1, :line_2, :city, :state, :pincode, :landmark]]
+  end
+
 end
